31aaf7bf9dd13b3b1856f003acb20d7b0da92efa
Rahul JAin <rahul.jain@appventurez.com>
2024-08-21 11:27:30 +0530

    add hedaer and footer components

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <p>Appventurez</p>
+            <ul>
+                <li>About</li>
+                <li>Service</li>
+                <li>Portfolio</li>
+                <li>Design</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
